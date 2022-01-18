28f065c9c79ee4a4ca966d725d868458225db1c0
sanhee <68936@naver.com>
2022-01-18 19:33:29 +0900

    프록시 v3 - 컴포넌트 스캔으로 스프링 빈 자동 등록

---
+++ src/main/java/com/example/springcoreadvanced/proxy/app/v3/OrderServiceV3.java
@@ -0,0 +1,14 @@
+package com.example.springcoreadvanced.proxy.app.v3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class OrderServiceV3 {
+    private final OrderRepositoryV3 orderRepository;
+
+    public void orderItem(String itemId) {
+        orderRepository.save(itemId);
+    }
+}
