7d4b89751a7f810560f57c717ea149bb1f0ab0b4
sanhee <68936@naver.com>
2022-01-18 19:30:40 +0900

    프록시 v2 - 인터페이스 없는 구체 클래스 - 스프링 빈으로 수동 등록

--- src/main/java/com/example/springcoreadvanced/proxy/ProxyApplication.java
+++ src/main/java/com/example/springcoreadvanced/proxy/ProxyApplication.java
@@ -1,11 +1,12 @@
 package com.example.springcoreadvanced.proxy;
 
 import com.example.springcoreadvanced.proxy.config.AppV1Config;
+import com.example.springcoreadvanced.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "com.example.springcoreadvanced.proxy.app")
 public class ProxyApplication {
     public static void main(String[] args) {
---
+++ src/main/java/com/example/springcoreadvanced/proxy/app/v2/OrderControllerV2.java
@@ -0,0 +1,27 @@
+package com.example.springcoreadvanced.proxy.app.v2;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping
+@ResponseBody
+public class OrderControllerV2 {
+    private final OrderServiceV2 orderService;
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v2/no-log")
+    public String noLog() {
+        return "ok";
+    }
+}
